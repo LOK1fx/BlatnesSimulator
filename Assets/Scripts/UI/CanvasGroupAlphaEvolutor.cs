2caa29fb06011c078bd26104d115b646207acd1d
Igor <lok1gameproject.suport@gmail.com>
2022-07-19 06:46:46 +0900

    Initial commit

---
+++ Assets/Scripts/UI/CanvasGroupAlphaEvolutor.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+namespace LOK1game.UI
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class CanvasGroupAlphaEvolutor : MonoBehaviour
+    {
+        [SerializeField] private float _speed;
+        [SerializeField, Range(0, 1)] private float _targetAlpha = 1;
+        [SerializeField, Range(0, 1)] private float _startAlpha = 1;
+
+        private CanvasGroup _canvas;
+
+        private void Awake()
+        {
+            _canvas = GetComponent<CanvasGroup>();
+        }
+
+        private void Start()
+        {
+            _canvas.alpha = _startAlpha;
+        }
+
+        private void Update()
+        {
+            _canvas.alpha = Mathf.Lerp(_canvas.alpha, _targetAlpha, Time.deltaTime * _speed);
+        }
+    }
+}
